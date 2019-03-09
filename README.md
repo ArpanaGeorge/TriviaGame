31a26ff18f3318f6752e90f23c45481dfa82d8ff
Arpana <arpanageorge@yahoo.in>
2019-03-08 23:33:51 -0800

    Completed Updated Files

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # TriviaGame
+
+## <b>Program Logic<b>
+<p>-->Stored all the questions in array queArray, answers in array answerArray and correct answer in array correctAnsArray.<br>
+
+-->On clicking Start button, Started the Quiz by calling fuction startQuiz()<br>
+
+-->Inside the startQuiz() function<br>
+        -Initialized all the variables for the start of each Quiz Questions <br>
+        -Using jQuery Got Question and answers from array and displayed it in the respective elements<br>
+        -Callied function timeRemFunction() every 1secs using setInterval method inorder to keep track of 30 secs timing for each Question<br>
+
+                -->function timeRemFunction()<br>
+                        -Decremented time by 1 <br>
+                        -used hover method to change colour of the answer options on hovering over it<br>
+                        -used click event listener - clicking will be either clicking correct answer or clicking wrong answer<br>
+
+                                -->Inside click event listener<br>
+                                        -Stopped calling of function timeRemFunction() every 1secs <br>
+                                        -if clicked on correct answer, Gave result for 3 seconds that it is correct and going to next round of Question<br>
+                                                -Kept track of number of correct answers<br>
+                                        -if clicked on wrong answer, Gave result for 3 seconds showing the correct answer and going to next round of Question<br>
+                                                -Kept track of number of wrong answers<br>
+                                                
+                        -If the Question number is equal to Question array length, then that is the end of Questions, so displaying final result - number of right , wrong and unanswered ones<br>
+                        -If not clicking on any answer, kept track of unanswered ones, displaying out of time and correct answer message and going to next round of Question<br>
---
+++ assets/javascript/app.js
@@ -0,0 +1,123 @@
+ $(document).ready(function()
+{
+    //Variable declarations
+    var i=-1, j=-4,t,time=30, Quenumbers, corectAnsArrayCounter=-1,clicked=0,start=0,correctAns=0,wrongAns=0,unanswered=0;
+    //Array declarations
+    var queArray = ["Who was the legendary Benedictine monk who invented champagne?","Name the largest freshwater lake in the world?","Where would you find the Sea of Tranquility?",
+    "What kind of weapon is a falchion?","What is the diameter of Earth?","What is another word for lexicon?"];
+    var answerArray = ["Gregor Mendel","Martin luther","Dom Perignon","Gregori Rasputin","Lake Tahoe","Lake Superior","Lake Marion","Lake Ladoga","Australia","Asia","Antartica",
+    "The Moon","sword","Spears","Bow and Arrow","Pike","10,000 miles","8000 miles","15,000 miles","12,000 miles","Computer","Digital","Dictionary","Semantics"];
+    var correctAnsArray = ["Dom Perignon","Lake Superior","The Moon","sword","8000 miles","Dictionary"]
+
+    $(".QASection").hide();
+    //Starting the Quiz by calling fuction startQuiz() on clicking Start button
+    $("#start").click(function(){
+        
+        $(".QASection").show();
+        $("#start").hide();
+        i=-1, j=-4,t,time=30, corectAnsArrayCounter=-1,correctAns=0,wrongAns=0,unanswered=0
+        startQuiz();
+    });
+    
+    //fuction startQuiz()
+    function startQuiz()
+    {
+        if(start === 0)
+        {
+           // alert("starting");
+            //Initializing all the variables for the start of each Quiz Questions 
+            start = 1;
+            clicked = 0;
+            i++;
+            Quenumbers=i;
+            j=j+4;
+            corectAnsArrayCounter++;
+            time = 30;
+            $(".QASection").show();
+            $("#result").hide();
+            //Getting Question and answers from array and displaying it in the respective elements
+            $("#Question").html(queArray[i]);
+            $("#Answer1").html(answerArray[j]);
+            $("#Answer2").html(answerArray[j+1]);
+            $("#Answer3").html(answerArray[j+2]);
+            $("#Answer4").html(answerArray[j+3]);
+            //Calling function timeRemFunction() every 1secs using setInterval method inorder to keep track of 30 secs timing for each Question
+            t = setInterval(timeRemFunction, 1000);
+        }
+    }
+
+    //function timeRemFunction()
+    function timeRemFunction()
+    {
+        //Decrementing time by 1 
+        $("#Time").html(time);
+        time--;
+        //Hover Event
+        $(".Answer").hover
+            (function(){
+                $(this).css({"background-color": "pink", "margin":"0px 250px", "border":"1px solid brown"});
+            },
+            function(){
+                $(this).css({"background-color": "rgb(0, 174, 255)","border":"0px"});
+            }
+        );
+
+        //click events - clicking will be either clicking correct answer or clicking wrong answer
+        $(".Answer").click(function(){
+            //Stopping calling of function timeRemFunction() every 1secs 
+            clearInterval(t);
+            $(this).css("background-color", "yellow");
+       
+            if(clicked===0)
+            {
+                //if clicked on correct answer, Giving result for 3 seconds that it is correct and going to next round of Question
+                if($(this).text() == correctAnsArray[corectAnsArrayCounter])
+                {        
+                    $(".QASection").hide();
+                    $("#result").html("That is Correct").show();
+                    restartQuiz();
+                    clicked=1;
+                    //Keeping track of number of correct answers
+                    correctAns++;
+                }
+                //if clicked on wrong answer, Giving result for 3 seconds showing the correct answer and going to next round of Question
+                else
+                {
+                    $(".QASection").hide();
+                    $("#result").html("That is wrong. Correct answer is "+correctAnsArray[corectAnsArrayCounter]).show();
+                    restartQuiz();
+                    clicked=1;
+                    //Keeping track of number of wrong answers
+                    wrongAns++;
+                }
+            }
+        });
+
+        //If the Question number is equal to Question array length, then that is the end of Questions, so displaying final result - number of right , wrong and unanswered ones
+        if(Quenumbers==queArray.length )
+        {
+            $(".QASection").hide();
+            $("#result").html("Final result"+"<br>"+"Correct Answer"+correctAns+"<br>"+"Wrong Answer"+wrongAns+"<br>"+"Unanswered"+unanswered).show();
+            $("#start").show();
+            $("#start").text("StartOver?");
+            start = 0;
+        }
+
+        //If not clicking on any answer, keeping track of unanswered ones, displaying out of time and correct answer message and going to next round of Question
+        if (time<0)
+        {
+            $(".QASection").hide();
+            $("#result").html("<br><b>Out of time</b><br>Correct answer was "+correctAnsArray[corectAnsArrayCounter]).show();
+            restartQuiz();
+           unanswered++;
+        }
+    }
+
+    //function restartQuiz()
+    function restartQuiz()
+    {
+        clearInterval(t);
+        start = 0;
+        setTimeout(function(){ startQuiz(); }, 3000);
+    }
+});
